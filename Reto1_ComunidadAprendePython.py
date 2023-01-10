2900e03c6a5b464d97adfc0c0c9fb89e223c1bf4
DavidLeeCUJAE <79540663+DavidLeeCUJAE@users.noreply.github.com>
2023-01-10 00:07:34 -0500

    Add files via upload

---
+++ Reto1_ComunidadAprendePython.py
@@ -0,0 +1,27 @@
+# Primer reto Nivel Facil de la Comunidad AprendePython (https://t.me/aprenderpython/30273/30286):
+# Crea un programa que lea la hora de tu pc y que cuando ejecutes el programa, te salude dependiendo la hora, ej
+# "Buen dia", "Buenas tardes" o "Buenas noches".
+# Realizado por:
+# ~Apuromafo (https://github.com/apuromafo/python_scripts/blob/main/script1.py)
+# Resuelto por: @DavidLeeCUJAE ()
+
+import time  # importar la librería time
+
+# definir una función para obtener las horas
+def get_time():
+    t = time.localtime()
+    horas = t[3]
+    return horas
+
+
+if __name__ == '__main__':
+    hora_actual = get_time()
+    # en dependencia de la hora cambio el saludo
+    if hora_actual < 6 or hora_actual > 18:
+        saludo = 'Buenas noches'
+    elif hora_actual > 6 and hora_actual < 12:
+        saludo = 'Buenos días'
+    elif hora_actual > 12 and hora_actual < 18:
+        saludo = 'Buenas tardes'
+    # muestro el saludo
+    print(f'{saludo}, la hora es {hora_actual}')
